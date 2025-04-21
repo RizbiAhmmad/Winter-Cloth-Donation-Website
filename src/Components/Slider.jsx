237e94c794281529faf8fd26db063b6a85524579
Rizbi ahmmad <contact.rizbi123@gmail.com>
2025-04-21 23:37:35 +0600

    Updating slider into banner

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,68 +1,26 @@
 import React from 'react';
 
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
-// Import required modules
-import { Navigation, Pagination, Mousewheel, Keyboard, Autoplay } from 'swiper/modules';
-
 const Slider = () => {
   return (
-    <Swiper
-      cssMode={true}
-      navigation={true}
-      pagination={{ clickable: true }}
-      mousewheel={true}
-      keyboard={true}
-      autoplay={{
-        delay: 3000, 
-        disableOnInteraction: false, 
+    <div
+      className="relative h-[500px] md:h-[700px] w-full bg-cover bg-center rounded-3xl overflow-hidden"
+      style={{
+        backgroundImage: `url('https://www.shutterstock.com/image-vector/clothes-donation-those-need-flat-600nw-2264436335.jpg')`,
       }}
-      modules={[Navigation, Pagination, Mousewheel, Keyboard, Autoplay]}
-      className="mySwiper"
     >
-      <SwiperSlide>
-        <img
-          className="md:h-[700px] w-full object-cover rounded-3xl"
-          src='https://www.shutterstock.com/shutterstock/photos/2374184363/display_1500/stock-vector-winter-clothes-donation-vector-illustration-in-flat-style-2374184363.jpg'
-          alt=""
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img
-          className="md:h-[700px] w-full object-cover rounded-3xl"
-          src='https://www.shutterstock.com/image-photo/female-volunteer-holding-donation-box-260nw-2109643052.jpg'
-          alt=""
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img
-          className="md:h-[700px] w-full object-cover rounded-3xl"
-          src='https://www.shutterstock.com/image-vector/winter-clothes-donationvector-illustration-flat-260nw-2374184361.jpg'
-          alt=""
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img
-          className="md:h-[700px] w-full object-cover rounded-3xl"
-          src='https://img.freepik.com/premium-photo/2-closeup-hands-passing-donation-box-filled-with-clothing-essentials-charity-worker-urban-setting_1295806-43252.jpg?semt=ais_hybrid'
-          alt=""
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img
-          className="md:h-[700px] w-full object-cover rounded-3xl"
-          src='https://www.shutterstock.com/image-vector/clothes-donation-those-need-flat-600nw-2264436335.jpg'
-          alt=""
-        />
-      </SwiperSlide>
-     
-    </Swiper>
+      {/* Optional Overlay */}
+      <div className="absolute inset-0 bg-black/50 z-10"></div>
+
+      {/* Text Content */}
+      <div className="relative z-20 flex flex-col items-center justify-center h-full text-center px-4 text-white">
+        <h1 className="text-3xl md:text-5xl font-bold mb-4">
+          Welcome to Donation Drive
+        </h1>
+        <p className="text-lg md:text-2xl max-w-2xl">
+          Join hands to make a difference — donate your clothes and bring warmth to someone’s life.
+        </p>
+      </div>
+    </div>
   );
 };
 
